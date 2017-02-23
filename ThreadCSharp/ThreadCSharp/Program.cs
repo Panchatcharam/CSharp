cc60e8398eb2f1da7f20433181447daf5b4ccd93
Panchatcharam <mrpanchu2003@gmail.com>
2017-02-23 23:49:17 +0800

    Initial Commit

---
+++ ThreadCSharp/ThreadCSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate worker thread object
+            WorkerThread wThread = new WorkerThread();
+
+            // Create thread, Here the ThreadStart is a delegate
+            Thread thrdObj = new Thread(new ThreadStart(wThread.HandleInput));
+
+            // Start the thread
+            thrdObj.Start();
+
+            Console.WriteLine("Wait for thread to become alive");
+
+            // Wait until thread to become alive
+            while (!thrdObj.IsAlive) ;
+
+            Console.WriteLine("Thread is alive");
+
+            // Wait for the thread to finish
+            thrdObj.Join();
+
+            Console.WriteLine("All is Well!, Press any key to Exit...");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ThreadCSharp/ThreadCSharp/WorkerThread.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace ThreadCSharp
+{
+    // A simple thread class for understaing the thread functionality.
+    class WorkerThread
+    {
+        // Thread function to handle the input from console and write
+        // to a file.
+        public void HandleInput()
+        {
+            string inputString;
+
+            // Use the using here to automatically call dispose
+            // This create file if its not created already.
+            using (StreamWriter writer = File.AppendText("log.txt"))
+            {
+                // Get the user input from the console
+                while ((inputString = Console.ReadLine()) != "")
+                {
+                    // Please not here that the using variable
+                    // can not be passed as `ref` or `out`. They 
+                    // are being treated as a read only.
+                    FormatAndWrite(ref inputString, writer);
+                    
+                    // Sleep for sometime for letting other threads if any to run.
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
+        private void FormatAndWrite(ref string message, TextWriter w)
+        {
+            w.WriteLine("{0} : {1}", DateTime.Now.ToString(),message);
+        }
+    }
+}
